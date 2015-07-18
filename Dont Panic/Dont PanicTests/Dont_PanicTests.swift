eb01556b4c15e476cffabf0e79157e7c37ef301c
Matt Ober <matt.ober3@gmail.com>
2015-07-18 11:41:58 -0500

    Initial Xcode project commit

---
+++ Dont Panic/Dont PanicTests/Dont_PanicTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dont_PanicTests.swift
+//  Dont PanicTests
+//
+//  Created by Matt on 7/18/15.
+//  Copyright © 2015 Gopher Studios. All rights reserved.
+//
+
+import XCTest
+@testable import Dont_Panic
+
+class Dont_PanicTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
